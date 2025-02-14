75caeea5fb6885d369e77329c0ce8181558fd8ef
unknown <wilfriedngassarou23@gmail.com>
2025-02-14 15:10:45 +0100

    Added constants

---
+++ src/constants/images.ts
@@ -0,0 +1,62 @@
+export const images = [
+  {
+    id: 'image-1',
+    src: 'images/1.webp',
+    angle: 0
+  },
+  {
+    id: 'image-2',
+    src: 'images/2.webp',
+    angle: Math.PI / 6
+  },
+  {
+    id: 'image-3',
+    src: 'images/3.webp',
+    angle: Math.PI / 3
+  },
+  {
+    id: 'image-4',
+    src: 'images/4.webp',
+    angle: Math.PI / 2
+  },
+  {
+    id: 'image-5',
+    src: 'images/5.webp',
+    angle: (2/3) * Math.PI
+  },
+  {
+    id: 'image-6',
+    src: 'images/6.webp',
+    angle: (5/6) * Math.PI
+  },
+  {
+    id: 'image-7',
+    src: 'images/7.webp',
+    angle: Math.PI
+  },
+  {
+    id: 'image-8',
+    src: 'images/8.webp',
+    angle: (7/6) * Math.PI
+  },
+  {
+    id: 'image-9',
+    src: 'images/5.webp',
+    angle: (4/3) * Math.PI
+  },
+  {
+    id: 'image-10',
+    src: 'images/2.webp',
+    angle: (3/2) * Math.PI
+  },
+  {
+    id: 'image-11',
+    src: 'images/3.webp',
+    angle: (5/3) * Math.PI
+  },
+  {
+    id: 'image-12',
+    src: 'images/4.webp',
+    angle: (11/6) * Math.PI
+  },
+] as const
